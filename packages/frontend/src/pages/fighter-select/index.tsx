3dcedd6f6514336123950938ece666dd878668f3
Anna <anysofronova@yandex.ru>
2023-03-24 18:47:44 +0300

    fix: images

--- packages/frontend/src/pages/fighter-select/index.tsx
+++ packages/frontend/src/pages/fighter-select/index.tsx
@@ -7,14 +7,17 @@ export const FighterSelect = () => {
   useEffect(() => {
     getImages().then(res => setCharacters(res))
   }, [])
-  console.log(characters)
   return (
     <MotionPage>
       <div className='flex h-screen w-screen items-center justify-center bg-[url(/public/background/bg.jpg)] bg-cover bg-center bg-no-repeat p-4'>
         <div className='grid h-full max-h-[600px] w-full max-w-[850px] grid-cols-5 grid-rows-3 items-center justify-center overflow-hidden'>
           {characters.map(character => (
             <div className='flex items-end justify-end' key={character.title}>
-              <img src={character.img} className='object-fit relative h-[200px] w-full bg-stone-800 shadow-sm' />
+              <img
+                src={character.img}
+                className='object-fit relative h-[200px] w-full bg-stone-800 shadow-sm'
+                alt={`Hero ${character.title}`}
+              />
             </div>
           ))}
         </div>
