acbfe750365ad2871beea206349eee879fb739ec
Anna <anysofronova@yandex.ru>
2023-03-24 18:45:13 +0300

    feat: images

--- packages/frontend/src/pages/fighter-select/index.tsx
+++ packages/frontend/src/pages/fighter-select/index.tsx
@@ -1,9 +1,28 @@
 import { MotionPage } from '@/shared/ui'
+import { useEffect, useState } from 'react'
+import { getImages, TCharactersObj } from '@/shared/lib/images/getImages'
 
 export const FighterSelect = () => {
+  const [characters, setCharacters] = useState<TCharactersObj[]>([])
+  useEffect(() => {
+    getImages().then(res => setCharacters(res))
+  }, [])
+  console.log(characters)
   return (
     <MotionPage>
-      <div className='h-screen w-screen bg-[url(/background/bg.jpg)] bg-cover bg-center bg-no-repeat'>fight</div>
+      <div className='flex h-screen w-screen items-center justify-center bg-[url(/public/background/bg.jpg)] bg-cover bg-center bg-no-repeat p-4'>
+        <div className='grid h-full max-h-[600px] w-full max-w-[850px] grid-cols-5 grid-rows-3 items-center justify-center overflow-hidden'>
+          {characters.map(character => (
+            <div className='flex items-end justify-end'>
+              <img
+                src={character.img}
+                className='object-fit relative h-[200px] w-full bg-stone-800 shadow-sm'
+                key={character.title}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </MotionPage>
   )
 }
