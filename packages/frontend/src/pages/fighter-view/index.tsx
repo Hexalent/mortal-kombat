6bb64ad564b481c881a16d1d6f853074794c1f46
balanced <vladislavherasymenko@gmail.com>
2023-03-25 01:44:15 +0200

    feat(CI/CD): Implement deployment workflows

--- packages/frontend/src/pages/fighter-view/index.tsx
+++ packages/frontend/src/pages/fighter-view/index.tsx
@@ -10,7 +10,7 @@ export const FighterView = () => {
         <div className='absolute -z-0 h-full w-full bg-[url(https://www.teahub.io/photos/full/171-1716638_mortal-kombat-fire-dragon-mortal-kombat.jpg)] bg-cover bg-center bg-no-repeat blur-sm' />
         <div className='relative z-10 flex h-full w-full items-end justify-between px-20'>
           <div className='absolute top-[50px] right-1/2 translate-x-1/2 font-russo text-8xl font-bold text-white'>
-            Battle 1
+            Battle
           </div>
           {selectedHeroes.map((hero, index) => (
             <motion.div
