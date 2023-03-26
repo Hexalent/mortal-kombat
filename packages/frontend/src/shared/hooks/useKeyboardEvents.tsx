f4faea3ce3783bce0c55a7a7f13aae8a055bbe7f
balanced <vladislavherasymenko@gmail.com>
2023-03-26 02:18:53 +0200

    feat: add `on` and `off` utils

--- packages/frontend/src/shared/hooks/useKeyboardEvents.tsx
+++ packages/frontend/src/shared/hooks/useKeyboardEvents.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect } from 'react'
 import { Character } from '@/shared/lib/images'
 import { heroesSelectors } from '@/entities'
+import { off, on } from '@/shared/utils'
 
 export const useKeyboardEvents = () => {
   const activeHero = heroesSelectors.use.activeHero()
@@ -21,13 +22,13 @@ export const useKeyboardEvents = () => {
         if (code === ACTIONS_MAP.APPLY && activeHero) selectHero(activeHero as Character)
       } else if (code === ACTIONS_MAP.ESCAPE) unselectLastHero()
     },
-    [activeHero, characters, selectHero, selectedHeroes, setActiveHero, unselectLastHero] // Add unselectLastHero to the dependency array
+    [activeHero, characters, selectHero, selectedHeroes, setActiveHero, unselectLastHero]
   )
 
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown)
+    on(document, 'keydown', handleKeyDown)
     return () => {
-      document.removeEventListener('keydown', handleKeyDown)
+      off(document, 'keydown', handleKeyDown)
     }
   }, [handleKeyDown])
 }
--- packages/frontend/src/shared/utils/index.ts
+++ packages/frontend/src/shared/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './named-lazy'
+export * from './util'
