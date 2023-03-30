f1245b82dbaf4e95971b259859c0a64414bf687f
Anna <anysofronova@yandex.ru>
2023-03-30 16:35:33 +0300

    fix: vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { promises as fs } from 'fs'
-import { join, resolve } from 'path'
-import { packageDirectorySync } from 'pkg-dir'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-const packageRoot = packageDirectorySync()
-
-async function getImageUrls() {
-  const backgroundDir = join(__dirname, 'public', 'background')
-  const charactersDir = join(__dirname, 'public', 'characters')
-
-  const backgroundFiles = await fs.readdir(backgroundDir)
-  const characterFiles = await fs.readdir(charactersDir)
-
-  const backgroundUrls = backgroundFiles.map(file => `/public/background/${file}`)
-  const characterUrls = characterFiles.map(file => `/public/characters/${file}`)
-
-  return [...backgroundUrls, ...characterUrls]
-}
-
-export default defineConfig(async () => {
-  const imageUrls = await getImageUrls()
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': resolve(packageRoot, './src')
-      }
-    },
-    build: {
-      rollupOptions: {
-        input: {
-          main: 'src/app/index.tsx',
-          sw: 'sw.ts'
-        }
-      }
-    },
-    define: {
-      'import.meta.imageUrls': JSON.stringify(imageUrls)
-    }
-  }
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()]
 })
