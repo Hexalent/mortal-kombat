014c5892c774f892a514841a440e3a342b278fef
Anna <anysofronova@yandex.ru>
2023-03-31 21:56:06 +0300

    fix: vercel deploy

--- src/shared/lib/images/getCharacters.tsx
+++ src/shared/lib/images/getCharacters.tsx
@@ -23,7 +23,7 @@ const buildCharacter = (data: CharacterData, index: number): Character => {
 }
 
 export const getCharacters = async (): Promise<Character[]> => {
-  const heroImages = import.meta.glob('/public/characters/*/*')
+  const heroImages = import.meta.glob('/characters/*/*')
   const images = await Promise.all(Object.values(heroImages).map(importImage => importImage()))
   const imagePaths: string[] = images.map((image: any) => image.default)
 
